0e1b84432e9ec3b627080d2645ae5c80acafce74
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-11 20:27:58 +0300

    added profile,project,comment class

--- award/admin.py
+++ award/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-# from .models import Editor,Article,tags
+from .models import Profile,Project,Comments
 # Register your models here.
 
 
 
-# admin.site.register(Editor)
-# admin.site.register(Article)
-# admin.site.register(tags)
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Comments)
