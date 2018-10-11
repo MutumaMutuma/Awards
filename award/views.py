addd68f513a6ca985d2144d06c6148421f3b4278
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-11 17:05:46 +0300

    added the app

---
+++ award/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
+
+# Create your views here.
+
+
+def welcome(request):
+    return render(request, 'welcome.html')
