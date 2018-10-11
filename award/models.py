23f301e4e243b3c5ce58fad06f13568f5844951e
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-11 20:07:59 +0300

    added profile class

--- award/models.py
+++ award/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=200)
+    profile_pic = models.ImageField(upload_to='profile/')
+    pub_date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profiles(cls):
+        profiles = cls.objects.all()
+        return profiles
+    
+    @classmethod
+    def search_by_username(cls,search_term):
+        profiles = cls.objects.filter(title__icontains=search_term)
+        return profiles
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
