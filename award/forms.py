c74c4e8a93b654c49099fe200ed589c506a4b976
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-12 14:15:42 +0300

    added signup

---
+++ award/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Project,Profile, Comments
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+# class ProjectForm(forms.ModelForm):
+#     class Meta:
+#         model = Project
+#         exclude = ['likes','profile','posted_time','profile','user']
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+# class CommentForm(forms.ModelForm):
+#     class Meta:
+#         model = Comments
+#         exclude = ['project','user']
+
+# class EditForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         fields = ['bio','profile_pic']
