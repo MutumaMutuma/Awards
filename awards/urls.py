8e52b34a4a67a2c872e4049b02726ead980c5717
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-15 11:00:49 +0300

    added url for profile

--- awards/urls.py
+++ awards/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from rest_framework.authtoken.views import obtain_auth_token
 from django.contrib.auth import views
 
 urlpatterns = [
@@ -22,5 +23,5 @@ urlpatterns = [
     url(r'',include('award.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}), 
-
+    url(r'^api/tokens',obtain_auth_token)
 ]
