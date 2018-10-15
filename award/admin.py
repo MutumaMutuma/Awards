9cadff9ae2174c27c862c645de2034b2440a3913
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-15 18:32:58 +0300

    added reviews

--- award/admin.py
+++ award/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Profile,Project,Comments
+from .models import Profile,Project,Reviews
 # Register your models here.
 
 
 
 admin.site.register(Profile)
 admin.site.register(Project)
-admin.site.register(Comments)
+admin.site.register(Reviews)
