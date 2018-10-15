d41398626b283cc20c797b7e13bcd9ec226234d9
MutumaMutuma <lewismutuma1000@gmail.com>
2018-10-15 18:34:35 +0300

    added a route to projects

--- award/urls.py
+++ award/urls.py
@@ -12,9 +12,10 @@ urlpatterns=[
     url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
     url(r'^new/project$', views.new_project, name='new-project'),
     url(r'^profile/$',views.profile,name='profile'),
-    
-    url(r'^edit/profile/$',views.edit_profile,name='edit_profile'),
+    url(r'^profiles/(\d+)',views.profiles,name='profiles'),
+    url(r'^projects/(\d+)',views.projects,name='projects'),
 
+    url(r'^edit/profile/$',views.edit_profile,name='edit_profile'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     url(r'^api/projects/$', views.ProjectList.as_view()),
